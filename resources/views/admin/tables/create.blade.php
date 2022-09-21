264b07c107b52ed022f8ddf1ffb81c51986aadab
hanif415 <hanifamrullah42@gmail.com>
2022-09-21 12:12:14 +0700

    commit

--- resources/views/admin/tables/create.blade.php
+++ resources/views/admin/tables/create.blade.php
@@ -26,24 +26,34 @@
                             @enderror
                         </div>
                         <div class="sm:col-span-6">
-                            <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
+                            <label for="guest_number" class="block text-sm font-medium text-gray-700"> Guest Number </label>
                             <div class="mt-1">
-                                <input type="file" id="image" name="image"
+                                <input type="file" id="guest_number" name="guest_number"
                                     class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') @enderror" />
                             </div>
-                            @error('image')
+                            @error('guest_number')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="sm:col-span-6 pt-5">
-                            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+                            <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
                             <div class="mt-1">
-                                <textarea id="description" rows="3" name="description"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') @enderror"></textarea>
+                                <select id="status" name="status[]" class="form-multiselect block w-full mt-1">
+                                    @foreach (App\Enums\TableStatus::cases() as $status)
+                                        <option value="{{ $status->value }}">${{ $status->name }}</option>
+                                    @endforeach   
+                                </select>
                             </div>
-                            @error('description')
-                                <div class="text-sm text-red-400">{{ $message }}</div>
-                            @enderror
+                        </div>
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
+                            <div class="mt-1">
+                                <select id="location" name="location[]" class="form-multiselect block w-full mt-1">
+                                    @foreach (App\Enums\TableLocation::cases() as $location)
+                                        <option value="{{ $location->value }}">${{ $location->name }}</option>
+                                    @endforeach   
+                                </select>
+                            </div>
                         </div>
                         <div class="mt-6 p-4">
                             <button type="submit"
