27766855f1e9645fadc08e3ab98acae25c8324b0
hanif415 <hanifamrullah42@gmail.com>
2022-10-09 14:18:21 +0700

    update menu controller index function

--- app/Http/Controllers/Frontend/WelcomeController.php
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $specials = Category::where('name', 'specials')->first();
+
+        return view('welcome', compact('specials'));
+    }
 }
